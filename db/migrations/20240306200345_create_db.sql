1b7e9446e8e42a6c58f25189e948b7fa6563dd50
Robert Usher <robertinthecloud@icloud.com>
2024-03-06 15:15:55 -0800

    some cleanup

--- db/migrations/20240306200345_create_db.sql
+++
@@ -1,8 +0,0 @@
--- Create "updated_timestamp" function
-CREATE FUNCTION "updated_timestamp" () RETURNS trigger LANGUAGE plpgsql AS $$
-BEGIN NEW.updated_at := CURRENT_TIMESTAMP;
-
-RETURN NEW;
-
-END;
-$$;
